c5c5b701715594989d178d68d5ddde6bf8a3b09f
Loudiya Surekha <lsurekha2002@gmail.com>
2025-06-15 17:35:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const arr = [4, 3, 2, 1];
+const arr = [1, 2, 3, 4, 1, 0, 2, 2];
 
 const divide = (arr, n) => {
   // Write your code here
@@ -22,9 +22,6 @@ const divide = (arr, n) => {
 	}  
 	return  result;
 };
-
-// console.log(divide([1, 2, 3, 4, 1, 0, 2, 2], 5));
-// console.log(divide([4, 3, 2, 1], 4));
 
 const n = prompt("Enter n: ");
 alert(JSON.stringify(divide(arr, n)));

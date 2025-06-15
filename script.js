e54ef5833ad8227f97781c7362e84517d3a43b20
Loudiya Surekha <lsurekha2002@gmail.com>
2025-06-15 17:31:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,30 @@
-const arr = [1, 2, 3, 4, 1, 0, 2, 2];
+const arr = [4, 3, 2, 1];
 
 const divide = (arr, n) => {
   // Write your code here
+		let result =[];
+	    let sum=0;
+	    let chunk=[];
+	for(var i=0; i<arr.length; i++){
+		let x=arr[i];
+		if(sum+x<=n){
+			chunk.push(x);
+			sum=sum+x;
+		}
+		else{
+			result.push(chunk);
+			chunk=[x];
+			sum=x;
+		}
+	}
+	if(chunk.length>0){
+		result.push(chunk);
+	}  
+	return  result;
 };
+
+// console.log(divide([1, 2, 3, 4, 1, 0, 2, 2], 5));
+// console.log(divide([4, 3, 2, 1], 4));
 
 const n = prompt("Enter n: ");
 alert(JSON.stringify(divide(arr, n)));
